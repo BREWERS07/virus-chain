caf8b7bf46e5e4a9556227303bcac829854d1ea7
BREWERS07 <92691896+BREWERS07@users.noreply.github.com>
2023-11-30 12:21:06 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+var codeText = document.getElementById("coupon-code");
+
+codeText.innerText = `Your special coupon code is ${Math.round(Math.random() * 100000000000000)}!!`;
+
+specialWindow = window.open("","Inspiration", 'width=400, height=400');
+specialWindow.document.body.innerHTML = `<div style="width: 100vw; height: 100vh; background-color: purple; background-size: 100% 100%;"></div>`;
