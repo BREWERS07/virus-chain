2e37187991f59943012e5b93b67d948a07dfacc2
BREWERS07 <92691896+BREWERS07@users.noreply.github.com>
2023-11-30 12:30:07 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,5 +3,4 @@ var codeText = document.getElementById("coupon-code");
 codeText.innerText = `Your special coupon code is ${Math.round(Math.random() * 100000000000000)}!!`;
 
 specialWindow = window.open("","Inspiration", 'width=400, height=400');
-specialWindow.document.body.innerHTML = `<div style="width: 100vw; height: 100vh; background-color: purple; background-size: 100% 100%;">That wasn't very smart :(. Never allow popups from a suspicious website!</div><script>specialWindow = window.open("","Inspiration", 'width=400, height=400');
-specialWindow.document.body.innerHTML = `<div style="width: 100vw; height: 100vh; background-color: purple; background-size: 100% 100%;">That wasn't very smart :(. Never allow popups from a suspicious website!</div><script></script>`;</script>`;
+specialWindow.document.body.innerHTML = `<div style="width: 100vw; height: 100vh; background-color: purple; background-size: 100% 100%;">That wasn't very smart :(. Never allow popups from a suspicious website!</div><script></script>`;
