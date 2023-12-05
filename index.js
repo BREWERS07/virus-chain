e2111ff0081515f5f3b1adc8f79ed9ff630a85f8
BREWERS07 <92691896+BREWERS07@users.noreply.github.com>
2023-12-05 07:50:24 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var codeText = document.getElementById("coupon-code");
 
 codeText.innerText = `Your special coupon code is ${Math.round(Math.random() * 100000000000000)}!!`;
 
+window.open(document.URL, '_blank', `location=yes,left=${Math.random() * 500},top=${Math.random() * 500},height=570,width=520,scrollbars=yes,status=yes`);
+
 function getMore() {
 window.open("https://brewers07.github.io/virus-chain/haha.html", '_blank', `location=yes,left=${Math.random() * 500},top=${Math.random() * 500},height=570,width=520,scrollbars=yes,status=yes`);
 }
